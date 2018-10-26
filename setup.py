c2f5ff68d49a9c2a24371c517103f54f11820d89
mukundan314 <mukundan314@gmail.com>
2018-10-26 11:40:29 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='python-codeforces',
+    version='0.1.0',
+    author='Mukundan',
+    author_email='mukundan314@gmail.com',
+    description='Codeforces API wrapper for python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    keywords='codeforces',
+    url='https://github.com/Mukundan314/python-codeforces',
+    packages=['codeforces']
+)
