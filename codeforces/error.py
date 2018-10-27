88f16a9215950e21e24b120964ac9f3970ae357e
mukundan314 <mukundan314@gmail.com>
2018-10-27 09:33:30 +0530

    Create error.py

---
+++ codeforces/error.py
@@ -0,0 +1,7 @@
+class CodeforcesAPIError(Exception):
+    def __init__(self, comment, method, args):
+        self.comment = comment
+        self.method = method
+        self.method_args = args
+
+        super(CodeforcesAPIError, self).__init__(comment)
