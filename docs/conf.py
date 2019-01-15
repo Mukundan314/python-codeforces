847d677f9dca0ae1f28d30034967a9533b083d41
mukundan314 <mukundan314@gmail.com>
2019-01-15 11:32:21 +0530

    chore: bump version

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Mukundan Senthil <mukundan314@gmail.com>'
 author = 'Mukundan Senthil <mukundan314@gmail.com>'
 
 # The short X.Y version
-version = ''
+version = '0.2'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
