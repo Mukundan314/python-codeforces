18f90142da89215af07fe06bd4123b7f8a3e4dcf
mukundan314 <mukundan314@gmail.com>
2019-01-16 07:48:12 +0530

    chore: Add classifiers in setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,13 @@ setup(
     python_requires='>=3.6,<4',
     install_requires=['bs4'],
     extras_requires={ 'docs': ['sphinx', 'sphinx_rtd_theme'] },
-    packages=find_packages(exclude=['docs'])
+    packages=find_packages(exclude=['docs']),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
 )
