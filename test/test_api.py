ab5700092017e94b73bcdf21411e3a1164e2b198
mukundan314 <mukundan314@gmail.com>
2019-01-17 07:55:22 +0530

    test: Add tests with pytest and tox

---
+++ test/test_api.py
@@ -0,0 +1,28 @@
+import pytest
+import codeforces
+
+class TestCall(object):
+    def test_undefined_method(self):
+        with pytest.raises(codeforces.error.CodeforcesAPIError):
+            method = "undefined.method"
+            codeforces.api.call(method)
+
+    def test_unauth_method(self):
+        method = "user.info"
+        handles = "python-codeforces"
+
+        codeforces.api.call(method, handles=handles)
+
+        key = "0d905168ea10217dd91472e861bf8c80932f060e"
+        secret = "3d3872085b0255159381a1884e9f66d5213ba796"
+        codeforces.api.call(method, key=key, secret=secret, handles=handles)
+
+    def test_auth_method(self):
+        method = "user.friends"
+
+        key = "0d905168ea10217dd91472e861bf8c80932f060e"
+        secret = "3d3872085b0255159381a1884e9f66d5213ba796"
+        codeforces.api.call(method, key=key, secret=secret)
+
+        with pytest.raises(codeforces.error.CodeforcesAPIError):
+            codeforces.api.call(method)
---
+++ test/test_problem.py
@@ -0,0 +1,14 @@
+import pytest
+import codeforces
+
+class TestGetInfo(object):
+    def test_normal_problem(self):
+        contest_id = 1
+        index = 'A'
+
+        title, time_limit, memory_limit, sample_tests = codeforces.problem.get_info(contest_id, index)
+
+        assert(title == 'A. Theatre Square')
+        assert(time_limit == '1 second')
+        assert(memory_limit == '256 megabytes')
+        assert(list(sample_tests) == [('6 6 4', '4')])
