047d4df071aac7dfc7d4265388a3c3868a19d27f
mukundan314 <mukundan314@gmail.com>
2019-01-17 12:57:40 +0530

    docs(codeforces.error): Add docstrings to codeforces/error.py

--- codeforces/error.py
+++ codeforces/error.py
@@ -1,6 +1,11 @@
+"""All exceptions used in codeforces."""
+
 __all__ = ['CodeforcesAPIError']
 
+
 class CodeforcesAPIError(Exception):
+    """Raised by api.call if method returns a error or is not found."""
+
     def __init__(self, comment, method, args):
         self.comment = comment
         self.method = method
