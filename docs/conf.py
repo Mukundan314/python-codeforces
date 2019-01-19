c4bdd0bff2e9ad48bbee4c3f4b6864dcc692f7e7
mukundan314 <mukundan314@gmail.com>
2019-01-19 23:13:58 +0530

    chore(version): Bump version (patch)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Mukundan Senthil <mukundan314@gmail.com>'
 # The short X.Y version
 version = '0.2'
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.2.3'
 
 
 # -- General configuration ---------------------------------------------------
